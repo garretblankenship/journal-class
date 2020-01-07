acb2ca13edf1a6f99b31772545547517561e3b99
garretblankenship <garret.blankenship@coderacademy.edu.au>
2020-01-08 10:52:56 +1100

    class

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,33 @@ import HomePage from "./pages/HomePage";
 import NewEntryPage from "./pages/NewEntryPage";
 
 class App extends Component {
+    state = { location: "home" }
+
+    getPage() {
+        const { location } = this.state;
+        switch(location) {
+            case "home":
+                return <HomePage />
+            case "categorySelection":
+                return <CategorySelectionPage />
+            case "newEntry":
+                return <NewEntryPage />
+            default:
+                return null;
+        }
+    }
+
+    changeLocation(location) {
+        this.setState({ location });
+    }
+    
     render() {
         return (
             <div>
-                <HomePage />
-                <CategorySelectionPage />
-                <NewEntryPage />
+                <button>Home</button>
+                <button>Category Selection</button>
+                <button onClick={} >New Entry</button>
+                {this.getPage()}
             </div>
         );
     }
